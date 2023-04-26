ef409ded5036a6b13a45224a06c709df5240c52c
Tim Crisp <timothycrisp@gmail.com>
2023-04-26 14:49:25 -0500

    install requirements, add rest framework to installed apps and url patterns

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('todos/', views.ToDoListCreateAPIView.as_view(), name='todo_list_create'),
     path('todos/<int:pk>/', views.ToDoRetrieveUpdateDestroyAPIView.as_view(), name='todo_retrieve_update_destroy'),
+    path('api-auth/', include('rest_framework.urls'))
 ]
