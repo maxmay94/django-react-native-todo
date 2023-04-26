275c0039478fd29708ba67fa348e7f09490479a4
Tim Crisp <timothycrisp@gmail.com>
2023-04-26 14:41:50 -0500

    stub up models, views, and urls, install rest framework package

---
+++ todo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('todos/', views.ToDoListCreateAPIView.as_view(), name='todo_list_create'),
+    path('todos/<int:pk>/', views.ToDoRetrieveUpdateDestroyAPIView.as_view(), name='todo_retrieve_update_destroy'),
+]
